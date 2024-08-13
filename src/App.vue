ba2960ee22b5cfab64f883bfb7ab68cd0a542825
KattyTotoro <89184036854@yandex.ru>
2024-08-13 20:57:35 +0300

    gdgf

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <form @submit.prevent="add">
+    <input v-model="message" placeholder="todo" />
+  </form>
+  <ul>
+    <li :data-check="el.check" v-for="el of arr" :key="el.id">{{ el.text }} <button @click="check(el.id,1)">✔</button><button  @click="check(el.id,2)">🤢</button></li>
+  </ul>
 </template>
 
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const message = ref('')
+const arr = ref([] as any[]) 
+
+const add = () => {
+  arr.value.push(
+    { text: message.value, id: arr.value.length, check: 0 },
+  )
+  message.value = ''
+}
+
+const check = (id:number, check:number) => {
+  const el = arr.value.find(el=>el.id==id)
+  el.check = check
+}
+
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+[data-check="1"] {
+  text-decoration: line-through;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+[data-check="2"] {
+  background-color: red;
 }
 </style>
