f0ac345e1b32a7196e473d29d7e6d8c3583bb537
KattyTotoro <89184036854@yandex.ru>
2024-09-04 01:06:16 +0300

    sdfsdf

--- src/components/Pictures.vue
+++ src/components/Pictures.vue
@@ -1,28 +1,30 @@
+
 <template>
 <!-- <div><img :src="" alt=""></div> -->
-<!-- <div class="img1"></div> -->
-<div>
+<div class="img1"></div>
+<!-- <div>
 <img :src="myImages">
-</div>
+</div> -->
+<img src="/images/apple.png" alt="">
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+// import { ref } from 'vue'
 
 
 
-const myImages = [
-{src:'images/apple.png'},
-// {},
-// {},
-// {},
-]
+// const myImages = [
+// {src:'images/apple.png'},
+// // {},
+// // {},
+// // {},
+// ]
 </script>
 
 <style scoped>
-/* .img1 {
-    background-image: url(images/apple.png);
+.img1 {
+    background: url(/images/apple.png) center/cover no-repeat;
    width: 100px;
     height: 100px;
-} */
+}
 </style>
