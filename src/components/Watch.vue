de0be024353bb3f419e2ccbdec8d9d5e93a7d632
KattyTotoro <89184036854@yandex.ru>
2024-09-08 23:43:55 +0300

    jkhkj

--- src/components/Watch.vue
+++ src/components/Watch.vue
@@ -2,26 +2,38 @@
 
 <div class="clouds">
     <div class="watch">
-      <div class="hours"><img src="/images/Hour.png" height="150px" alt=""></div>
-      <div class="minutes"><img src="/images/minutes.png" height="180px" alt=""></div>
-      <div class="seconds"><img src="/images/seconds.png" height="230px" alt=""></div>
+      <div class="hours" :style="'transform: rotate(' + num_hours + 'deg);'"><img src="/images/Hour.png" height="150px" alt=""></div>
+      <div class="minutes" :style="'transform: rotate(' + num_minutes + 'deg);'"><img src="/images/minutes.png" height="180px" alt=""></div>
+      <div class="seconds" :style="'transform: rotate(' + num_seconds + 'deg);'"><img src="/images/seconds.png" height="230px" alt=""></div>
+      <!-- <div class="seconds"><img src="/images/seconds.png" height="230px" alt=""></div> -->
     </div>
   </div>
-
+<!-- 
     <h1>{{hours}}</h1>
     <h1>{{minutes}}</h1>
+    <h1>{{seconds}}</h1>
+    <h1>{{num_hours}}</h1> -->
 
 </template>
 
 <script setup lang="ts">
+import { transform } from 'typescript';
 import { ref } from 'vue'
+let num_hours = ref(0) as any
+let num_minutes = ref(0) as any
+let num_seconds = ref(0) as any
 
 let date = new Date()
 let hours = date.getHours()
-hours = (hours > 12) ? hours = hours - 12 : hours
-const minutes = date.getMinutes()
+hours = hours % 12
+// hours = (hours > 12) ? hours = hours - 12 : hours
+let minutes = date.getMinutes()
+let seconds = date.getSeconds()
+num_seconds.value = seconds * 360 / 60
+num_minutes.value = (minutes * 360 / 60) + (seconds * 0.1)
+num_hours.value = (hours * 360 / 12) + ((minutes * 60) * 0.0017)
 
-// x градусов = hours * 360 / 12
+
 
 </script>
 
@@ -55,7 +67,8 @@ const minutes = date.getMinutes()
       left: calc(47% - (3px/2));
       top: 112px;
       transform-origin: 50% 86%;
-      animation: time 86400s linear infinite;
+      transform: rotate(0);
+      animation: time 43200s linear infinite;
     }
 
     .minutes {
@@ -75,17 +88,11 @@ const minutes = date.getMinutes()
       /* animation: time 60s steps(60) infinite; */
     }
 
-    @keyframes time {
-      from {
-        transform: rotate(0);
-      }
-
-      to {
+    @keyframes time {      
+    to {
         transform: rotate(360deg);
       }
     }
-
-   
 
     @keyframes cloud {
       from {
