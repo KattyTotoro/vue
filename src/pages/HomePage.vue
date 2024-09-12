3b2eea6976e668686f1c0412a9743a37ba9f25ba
KattyTotoro <9184036854@yandex.ru>
2024-09-12 20:05:34 +0300

    gdgf

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,50 @@
+<template>
+  <h1>Home</h1>
+  <TrafficLight></TrafficLight>
+  <Pictues></Pictues>
+  <CallUs></CallUs>
+  <Watch></Watch>
+  <form @submit.prevent="add">
+    <input v-model="message" placeholder="todo" />
+  </form>
+  <ul>
+    <li :data-check="el.check" v-for="el of arr" :key="el.id">{{ el.text }} <button
+        @click="check(el.id, 1)">✔</button><button @click="check(el.id, 2)">🤢</button></li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import TrafficLight from '../components/TrafficLight.vue'
+import Pictues from '../components/Pictures.vue'
+import CallUs from '../components/CallUs.vue';
+import Watch from '../components/Watch.vue';
+
+const message = ref('')
+const arr = ref([] as any[])
+
+const add = () => {
+  arr.value.push(
+    { text: message.value, id: arr.value.length, check: 0 },
+  )
+  message.value = ''
+}
+
+const check = (id: number, check: number) => {
+  const el = arr.value.find(el => el.id == id)
+  el.check = check
+
+
+}
+</script>
+
+<style scoped>
+[data-check="1"] {
+  text-decoration: line-through;
+}
+
+[data-check="2"] {
+  background-color: red;
+
+}
+</style>
