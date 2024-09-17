875a02ad88433965bf6d080798f8304ed5666b94
KattyTotoro <89184036854@yandex.ru>
2024-09-17 15:00:37 +0300

    dfsdsf

--- src/components/MilesToKilomConverter.vue
+++ src/components/MilesToKilomConverter.vue
@@ -1,27 +1,64 @@
-
-// конвертор из километров в мили (пользователь вводит километры, программа выводит мили). 
-// 1 км = 0,621371 миль. 
+// конвертер из километров в мили (пользователь вводит километры, программа выводит мили).
+// 1 км = 0,621371 миль.
 
 <template>
 
-    
-
+<div>
+<h3>Конвертер из километров в мили</h3>
+  <div class="converter">
+    <div>
+      <p>Введите значение в километрах:</p>
+      <input class="userImput" type="text">
+    </div>
+    <div>
+      <p>Результат в милях:</p>
+      <p class="result">{{ mileage }}</p>
+    </div>
+  </div>
+</div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
+let distanceInKm = ref(0) as any
+let mileage = ref(0) as any
 
-// const KILOMETERS_IN_ONE_MILE = 1.609344
+const KILOMETERS_IN_ONE_MILE = 1.609344
 
-// let distanceInKm = 2
+distanceInKm = 2
 
-// let mileage = distanceInKm / KILOMETERS_IN_ONE_MILE
+mileage = distanceInKm / KILOMETERS_IN_ONE_MILE
 
-// console.log(distanceInKm, 'км равно ', mileage, 'миль')
+console.log(distanceInKm, 'км равно ', mileage, 'миль')
 
 
 </script>
 
 <style scoped>
+.converter {
+  display: flex;
+  align-content: center;
+  justify-content: space-evenly;
+  align-items: baseline;
+  border: 2px solid darkgreen;
+  margin-bottom: 500px;
+}
 
+.converter div:last-child {
+  border-left: 2px solid darkgreen;
+  padding-left: 20px;
+}
+
+h3 {
+  margin-top: 100px;
+}
+
+.result {
+  padding: 5px;
+  border: 1px solid black;
+}
+
+.userImput {
+  padding: 6px;
+}
 </style>
--- src/pages/MyNewPage.vue
+++ src/pages/MyNewPage.vue
@@ -1,10 +1,12 @@
 <template>
     <h1>My new page</h1>
+  <MilesToKilomConverter></MilesToKilomConverter>
   <Watch></Watch>
 
 </template>
 
 <script setup lang="ts">
+import MilesToKilomConverter from '#root/components/MilesToKilomConverter.vue';
 import Watch from '../components/Watch.vue';
 
 
